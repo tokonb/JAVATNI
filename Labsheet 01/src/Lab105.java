aa1185d8a4d2abf9c657e850172a9bfd44b9c1d4
tokonb <tonkhun123@gmail.com>
2025-11-17 17:21:44 +0700

    commit Labsheet 01.java

---
+++ Labsheet 01/src/Lab105.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Summation number = " + (2+5) + "\nSubtraction number = " + (-5+2) + "\nMultiplication number = " + (2*5) + "\nDivision number = " + (2/5) + "\nModulus number = 2",
+                "Arithmetic Operators",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
